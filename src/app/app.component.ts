f0d233ddc78ba7c8d22d23616177b7107ecdbd85
Juandieruiz <juandieruiz6@gmail.com>
2022-01-11 11:01:44 +0100

    interpolación-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'Interpolación';
+  img =
+    'https://thecatapi.com/api/images/get?format=src&type=gif?results_per_page=';
 }
